4c5bc48edec4b3a5d65afae50d225228b7931cf0
Svetoslav Krastev <svetoslav.tkrastev@gmail.com>
2020-04-08 20:54:09 +0300

    finished with the mockup

--- hidash/index.test.js
+++ hidash/index.test.js
@@ -1,10 +1,19 @@
-const { forEach } = require('./index')
+const assert = require('assert')
+const { forEach, map } = require('./index')
 
-let sum = 0
-forEach([1,2,3], (value) => {
-  sum += value
+it('The forEach function', () => {
+  let sum = 0
+  forEach([1, 2, 3], (value) => {
+    sum += value
+  })
+
+  assert.strictEqual(sum, 6, 'Expected the correct sum of the array')
 })
 
-if (sum !== 6) {
-  throw new Error('Expected summing array to equal 6')
-}
+it('The map function', () => {
+  const result = map([1, 2, 3], value => {
+    return value * 2
+  })
+
+  assert.deepStrictEqual(result, [2, 4, 6])
+})
